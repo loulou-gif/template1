699bf9dd20b0a6a9cb333b41b788ff78731ac17c
unknown <konankanjulius10@gmail.com>
2024-01-18 20:25:07 +0000

    Sections4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Section1 from '../components/Section1';
 import Header from '../components/Header';
 import Section2 from '../components/Section2';
+import Section4 from '../components/Section4';
 
 const Home = () => {
     return(
@@ -8,6 +9,7 @@ const Home = () => {
             <Header/>
             <Section1/>
             <Section2/>
+            <Section4/>
         </div>
     )
 }
