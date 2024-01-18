8cd10aaa48784da178c7abd883c1ce4a8886580e
unknown <konankanjulius10@gmail.com>
2024-01-18 20:19:55 +0000

    secteur2 end

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,15 +1,21 @@
+import can1 from "../assets/img/can1.jpg"
+import "../assets/css/Styles.css"
 const Section2 =() => {
     return(
-        <section className="section2 h-80 w-full flex justify-center bg-black mt-36">
-            <div className="">
-                <div className="flex border justify-center text-white mb-10 mt-10 w-10/12">
-                    <h3 className="">Allez les éléphants</h3>
-                    <img alt="" />
+        <section className="section2 h-full pb-10 w-full flex justify-center bg-black mt-36">
+            <div className="w-5/12  mt-5 ">
+                <h2 className="mt-5 text-white  w-6/12 ml-10 text-5xl font-semibold" >TOUS AVEC LES ELEPHANTS</h2>
+                <p className="mt-5 text-white  w-11/12 ml-10 text-xl ">Le président ivoirien a investi des millions dans la construction et la rénovation de plusieurs stades, mais ce n'est pas tout. Une préparation exceptionnelle a été entreprise pour offrir des conditions optimales à nos éléphants.<br/>
+                Le président ivoirien a investi des millions dans la construction et la rénovation de plusieurs stades, mais ce n'est pas tout. Une préparation exceptionnelle a été entreprise pour offrir des conditions optimales à nos éléphants.</p>
+                <img className="w-11/12 ml-10 h-4/12 mt-5 rounded-lg" alt="" src={can1} />
+            </div>
+            <div className="w-5/12  mt-5 ">
+                <p className="mt-5 text-white  w-11/12 ml-10 text-xl ">Le président ivoirien a investi des millions dans la construction et la rénovation de plusieurs stades, mais ce n'est pas tout. Une préparation exceptionnelle a été entreprise pour offrir des conditions optimales à nos éléphants.<br/>
+                Le président ivoirien a investi des millions dans la construction et la rénovation de plusieurs stades, mais ce n'est pas tout. Une préparation exceptionnelle a été entreprise pour offrir des conditions optimales à nos éléphants.</p>
+                <div className="w-11/12 ml-10 h-8 flex justify-end">
+                    <div className="w-64 bg-white h-8 "></div>
                 </div>
-                <div className="flex border justify-center text-white mb-10 mt-10 w-10/12">
-                    <h3 className="">Allez les éléphants</h3>
-                    <img className="w-10" alt=""  />
-                </div>
+                <img className="pictures w-11/12 ml-10 h-4/12 rounded-lg" alt="" src={can1} />
             </div>
 
         </section>
