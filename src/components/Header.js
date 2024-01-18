884bf9fd36147db4cf577347a1e5cdd83c19cc15
unknown <konankanjulius10@gmail.com>
2024-01-18 23:02:30 +0000

    all sections

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import '../assets/css/Styles.css'
 const Header = () => {
    return (
         <header className="header border text-white">
-            <div className=' w-4/12 min-w-96 ml-64 mt-28'>
-                <h1 className=' font-bold ml-5  text-2xl '>AKWABA A LA CAN DE L'HOSPITALITE</h1>
-                <div className='ml-5 h-2 flex justify-end'>
-                    <div className='ml-5 w-6/12 h-1 bg-white' ></div>
+            <div className=' w-5/12 min-w-96 ml-40 mt-28'>
+                <h1 className=' font-bold   text-2xl '>AKWABA A LA CAN DE L'HOSPITALITE</h1>
+                <div className='w-8/12 h-2'>
+                    <div className='w-6/12 h-1 bg-white' ></div>
                 </div>
-                <p className='ml-5 font-semibold text-7xl'>La terre de l'hospitalité est ravi de vous accueillir sur son térritoire pour célébrer la fête du football africain</p>
+                <p className=' font-semibold text-7xl'>La terre de l'hospitalité est ravi de vous accueillir sur son térritoire pour célébrer la fête du football africain</p>
             </div>
         </header>
     )
