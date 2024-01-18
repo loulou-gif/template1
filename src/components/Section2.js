cf1db760cad93e4bae82d80d6f7616072c260f6b
unknown <konankanjulius10@gmail.com>
2024-01-18 19:02:33 +0000

    secteur2

---
+++ src/components/Section2.js
@@ -0,0 +1,19 @@
+const Section2 =() => {
+    return(
+        <section className="section2 h-80 w-full flex justify-center bg-black mt-36">
+            <div className="">
+                <div className="flex border justify-center text-white mb-10 mt-10 w-10/12">
+                    <h3 className="">Allez les éléphants</h3>
+                    <img alt="" />
+                </div>
+                <div className="flex border justify-center text-white mb-10 mt-10 w-10/12">
+                    <h3 className="">Allez les éléphants</h3>
+                    <img className="w-10" alt=""  />
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Section2 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import Section1 from '../components/Section1';
 import Header from '../components/Header';
+import Section2 from '../components/Section2';
 
 const Home = () => {
     return(
         <div className="home">
             <Header/>
             <Section1/>
+            <Section2/>
         </div>
     )
 }
