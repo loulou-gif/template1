2889bcaaaf59366ca3c16418a43da17116468fd1
unknown <konankanjulius10@gmail.com>
2024-01-18 13:12:59 +0000

    header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+import '../assets/css/Styles.css'
+
+const Header = () => {
+   return (
+        <header className="header border text-white">
+            <div className=' w-3/12 min-w-96 ml-64 mt-28'>
+                <h1 className=' font-bold text-center text-xl mb-5 border-b-4'>AKWABA A LA CAN DE L'HOSPITALITE</h1>
+                <p className='ml-5 font-semibold text-5xl'>La terre de l'hospitalité est ravi de vous accueillir sur son térritoire pour célébrer la fête du football africain</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
+import Header from '../components/Header';
+
 const Home = () => {
     return(
-        <div>
-            lol
+        <div className="home">
+            <Header/>
         </div>
     )
 }
