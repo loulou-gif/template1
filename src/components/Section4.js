0f6384ec78f108a4774dae30d4efa607f8520cb8
unknown <konankanjulius10@gmail.com>
2024-01-18 21:06:14 +0000

    Sections4

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -7,15 +7,15 @@ const Section4 =() => {
                 <h2 className="mt-5 text-white  w-6/12 ml-10 text-5xl font-semibold" >TOUS AVEC LES ELEPHANTS</h2>
                 <p className="mt-5 text-white  w-11/12 ml-10 text-xl ">Le président ivoirien a investi des millions dans la construction et la rénovation de plusieurs stades, mais ce n'est pas tout. Une préparation exceptionnelle a été entreprise pour offrir des conditions optimales à nos éléphants.<br/>
                 Le président ivoirien a investi des millions dans la construction et la rénovation de plusieurs stades, mais ce n'est pas tout. Une préparation exceptionnelle a été entreprise pour offrir des conditions optimales à nos éléphants.</p>
-                <img className="w-11/12 ml-10 h-4/12 mt-5 rounded-lg" alt="" src={can1} />
+                <img className="pictures ml-10 h-4/12 mt-5 rounded-lg" alt="" src={can1} />
             </div>
             <div className="w-5/12  mt-5 ">
                 <p className="mt-5 text-white  w-11/12 ml-10 text-xl ">Le président ivoirien a investi des millions dans la construction et la rénovation de plusieurs stades, mais ce n'est pas tout. Une préparation exceptionnelle a été entreprise pour offrir des conditions optimales à nos éléphants.<br/>
                 Le président ivoirien a investi des millions dans la construction et la rénovation de plusieurs stades, mais ce n'est pas tout. Une préparation exceptionnelle a été entreprise pour offrir des conditions optimales à nos éléphants.</p>
-                <div className="w-11/12 ml-10 h-5 flex justify-end">
+                <div className="w-11/12 ml-10 h-5 flex justify-end mb-28">
                     <div className="w-64 bg-white h-5 "></div>
                 </div>
-                <img className="pictures w-11/12 ml-10 h-4/12 rounded-lg" alt="" src={can1} />
+                <img className="pictures w-11/12 ml-10  rounded-lg" alt="" src={can1} />
             </div>
 
         </section>
